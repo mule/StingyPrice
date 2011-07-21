56d0b7118f06a269b474347ed6bc03d3aa554e99
Jukka Puranen <jukka.puranen@houston-inc.com>
2011-07-21 17:53:04 +0300

    working on ui

---
+++ StingyPrice/Controllers/CategoriesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StingyPrice.DAL.Repositories;
+
+namespace StingyPrice.Controllers
+{
+    public class CategoriesController : Controller
+    {
+      private IRepository _repository;
+
+      public CategoriesController()
+      {
+        
+      }
+
+      public CategoriesController(IRepository repository)
+      {
+        _repository = repository;
+
+      }
+        //
+        // GET: /Categories/
+
+        public ActionResult Index()
+        {
+          
+
+            return View();
+        }
+
+    }
+}
