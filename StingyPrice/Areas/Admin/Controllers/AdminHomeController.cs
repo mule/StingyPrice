46b044abbbbed12a5cb13307f866c20071cf4b98
Jukka Puranen <jukka.puranen@houston-inc.com>
2011-08-10 12:36:00 +0300

    Fixed admin area home controller and views

--- StingyPrice/Areas/Admin/Controllers/AdminHomeController.cs
+++ StingyPrice/Areas/Admin/Controllers/AdminHomeController.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using StingyPriceDAL.Repositories;
 
 namespace StingyPrice.Areas.Admin.Controllers
 {
     public class AdminHomeController : Controller
     {
+      private IRepository _repository;
+
+
+      public AdminHomeController(IRepository repository)
+      {
+        _repository = repository;
+
+      }
         //
         // GET: /Admin/Home/
 
